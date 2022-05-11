e30f60dbdc9ae279cf22dfdbc9888b945ccc46b1
Piyush Raj Bista <piyushbista@gmail.com>
2022-05-12 03:37:58 +0545

    init

---
+++ src/main/java/com/foodeo/backend/Foodeo/FoodeoApplication.java
@@ -0,0 +1,13 @@
+package com.foodeo.backend.Foodeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodeo/backend/Foodeo/FoodeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodeo.backend.Foodeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
